c4b8621e34141f89c4d1f5a32f039e38ca79f7e5
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-11-25 18:51:03 -0500

    REMOVE: App.css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,6 @@ import Header from './Header';
 import Main from '@/components/Main';
 import Footer from '@/components/Footer';
 
-import '@/App.css';
-
 function App() {
 	return (
 		<div className='App flex h-full w-full flex-col items-center justify-between'>
