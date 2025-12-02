eef12561bc7469995046ce070553a3203deae263
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-12-02 15:32:52 -0500

    ADD: SVG Support (Vite Plugin)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,11 @@
+import LogoIcon from '@/assets/logo.svg?react';
+
 function Logo() {
-	return <div className='Logo'>Logo</div>;
+	return (
+		<div className='Logo' id='epk-logo'>
+			<LogoIcon aria-label='Old Mountain Acid Test Logo' />
+		</div>
+	);
 }
 
 export default Logo;
